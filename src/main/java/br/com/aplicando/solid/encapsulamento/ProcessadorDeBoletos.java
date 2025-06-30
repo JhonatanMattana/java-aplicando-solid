991c54d8cd570382ea3df822f2629296cc750a5a
jhonatan.mattana <jhonatan.mattana@maxiconsystems.com.br>
2025-06-30 20:51:12 -0300

    Encapsulamento: solução

--- src/main/java/br/com/aplicando/solid/encapsulamento/ProcessadorDeBoletos.java
+++ src/main/java/br/com/aplicando/solid/encapsulamento/ProcessadorDeBoletos.java
@@ -4,17 +4,10 @@ import java.util.List;
 
 public class ProcessadorDeBoletos {
     public void processa(List<Boleto> boletos, Fatura fatura) {
-
-        double total = 0;
-        for(Boleto boleto : boletos) {
-            Pagamento pagamento = new Pagamento(boleto.getValor(), MeioDePagamento.BOLETO);
-            fatura.getPagamentos().add(pagamento);
-
-            total += boleto.getValor();
-        }
-
-        if(total >= fatura.getValor()) {
-            fatura.setPago(true);
+        for (Boleto boleto : boletos) {
+            Pagamento pagamento = new Pagamento(boleto.getValor(),
+                    MeioDePagamento.BOLETO);
+            fatura.adicionaPagamento(pagamento);
         }
     }
 }
